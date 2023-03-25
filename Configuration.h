38bead4bceae998d622c3986c575be6dfd901d11
AgustinBaffo <agusbaffo@hotmail.com>
2023-03-25 12:16:27 -0300

    update nozzle-extruder distances

--- Configuration.h
+++ Configuration.h
@@ -1486,7 +1486,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -52, -11, 0 } // Baffer: aprox
+#define NOZZLE_TO_PROBE_OFFSET { 57, -14, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
